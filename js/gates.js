20754f84e27c9015ce238dbd1eb208033390b04f
Nick Mealey <mealey@partstash.com>
2014-12-22 13:54:14 -0800

    Added method to add a gate.

--- js/gates.js
+++ js/gates.js
@@ -33,14 +33,31 @@
       }
       
       return self.template;
-    }
+    };
+    
+    // Create a gate
+    this.newGate = function(path, template){
+      // Check if a template was given, if not set from default
+      if(template){
+        template = new Template(template);
+      } else {
+        template = self.defaultTemplate();
+      }
+      // Create a new gate
+      var gate = new Gate(path, template);
+      
+      // Push it to gates
+      this.gates.push(gate);
+    };
     
     // Setup basic routes
     this.routes = function(routes){
       for (var i=0; i < routes.length; i++){
-        var route = routes[i];
+        var path = routes[i];
         var template = this.defaultTemplate();
-        this.gates.push(new Gate(route, template));
+        
+        // Create the gate
+        self.newGate(path, template);
       };
     };
   };
