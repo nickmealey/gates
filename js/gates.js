aeba8810f7789a5e6a042d26bc27423bc66c70f6
Nick Mealey <mealey@partstash.com>
2014-12-22 11:57:39 -0800

    Added basic file structure.

---
+++ js/gates.js
@@ -0,0 +1,12 @@
+/*
+ Gates
+ Author: Nick Mealey
+*/
+
+(function($){
+  if (!$){
+    throw "Gates requires jQuery as a dependency";
+  } else {
+    console.log(window.location);
+  }
+})(jQuery);
